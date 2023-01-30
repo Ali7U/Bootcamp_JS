4a31f1a555784da8a50ed3f9be15de9556ce3f03
Ali <alipro.ig7@gmail.com>
2023-01-30 10:47:35 +0300

    work in bootcamp

---
+++ week_3/day1/main.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var fname = "Ali";
+// fname = 3
+var arr = [1, "Ali", 22];
+arr = [3, 5];
---
+++ week_3/day1/main.ts
@@ -0,0 +1,7 @@
+let fname = "Ali";
+// fname = 3
+let arr: (number | string)[] = [1, "Ali", 22];
+
+arr = [3, 5]
+
+export {};
